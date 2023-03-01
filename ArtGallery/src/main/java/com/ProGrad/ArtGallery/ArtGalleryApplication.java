a90f8cb2090367a1c7140d22149e1f7a2276416f
Rahul Chhabra <chhabrarahul2707@gmail.com>
2023-03-01 20:58:41 +0530

    Initial setup done.

---
+++ ArtGallery/src/main/java/com/ProGrad/ArtGallery/ArtGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.ProGrad.ArtGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGalleryApplication.class, args);
+	}
+
+}
